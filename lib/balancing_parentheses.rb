93e7b166e7d1efde8c68cbd8d18afca4273f2ead
JordanJoseph <jordant.joseph@outlook.com>
2023-01-16 11:57:46 -0700

    commit tree 3d9db3f8bad13978bd0dbb7cc70a4bd5c846df2a

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,34 @@
 require_relative './stack'
 
 # your code here
+
+def balancing_parentheses(string)
+    value = 0
+    newStack = Stack.new 
+
+    left = '('
+    right = ')'
+
+    string.each_char do |ch|
+        if left == ch
+            newStack.push(ch)
+        elsif right == ch
+            ch == newStack.peek ? newStack.pop : value+=1
+        end 
+    end 
+
+    value
+
+end 
+
+    #newStack.push(string)
+    
+    #while newStack.size > 0 do 
+        
+    #    if newStack.pop == '('
+    #        left += 1
+    #    elsif newStack.pop == ')'
+    #        right += 1
+    #    end
+
+    #end
